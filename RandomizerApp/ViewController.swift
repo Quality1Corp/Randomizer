0ecda336f4a0e8970134e16dc8eb1fc439e2f15d
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-04-02 15:10:11 +0300

    user interface layout

--- RandomizerApp/ViewController.swift
+++ RandomizerApp/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var minimumValueLabel: UILabel!
+    @IBOutlet var maximumValueLabel: UILabel!
+    @IBOutlet var randomValueLabel: UILabel!
+    @IBOutlet var getRandomNumberButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getRandomNumberButton.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func getRandomNumberButtonTapped() {
+        
+        let minimumNumber = Int(minimumValueLabel.text ?? "") ?? 0
+        let maximumNumber = Int(minimumValueLabel.text ?? "") ?? 100
+        
+        randomValueLabel.text = Int.random(in: minimumNumber...maximumNumber).formatted()
+    }
+    
 }
 
